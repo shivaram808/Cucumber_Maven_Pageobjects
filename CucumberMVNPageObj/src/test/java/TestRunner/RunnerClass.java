5b608f32e74c40668af2cfe5b355cab32fb24cf8
shiva <screens.ems@gmail.com>
2019-05-05 22:54:57 +0530

    cucumber reports updated

--- CucumberMVNPageObj/src/test/java/TestRunner/RunnerClass.java
+++ CucumberMVNPageObj/src/test/java/TestRunner/RunnerClass.java
@@ -4,7 +4,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
-@CucumberOptions(plugin = {"html:target/CucumberReports"},features=".\\src\\test\\resources\\features", glue = {"stepDefinition"})
+@CucumberOptions(plugin = {"html:target/CucumberReports","json:target/cucumber.json",
+"pretty:target/cucumber-pretty.txt","junit:target/cucumber-results.xml"},
+features=".\\src\\test\\resources\\features", glue = {"stepDefinition"})
 public class RunnerClass extends AbstractTestNGCucumberTests{
 
 
